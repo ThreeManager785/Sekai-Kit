4f0c2dca3d1161feb1ee4e2d4feea786d88cc512
ThreeManager785 <leimeichun50@gmail.com>
2025-10-19 14:27:21 +1000

    Updated Branch Determination Algorithm

--- OfflineResBundleGen/InfoDetermination.swift
+++ OfflineResBundleGen/InfoDetermination.swift
@@ -15,28 +15,6 @@
 import DoriKit
 import Foundation
 
-func analyzePathBranch(_ path: String) -> String {
-    if pathIsInUnavailableBranch(path) {
-        return "unsupported"
-    } else if path.hasPrefix("video") {
-        return "video"
-    } else if path.hasPrefix("sound") {
-        return "sound"
-    } else {
-        return "basic"
-    }
-    
-    func pathIsInUnavailableBranch(_ path: String) -> Bool {
-        let unavailablePaths = ["characters/ingameresourceset", "live2d", "musicscore", "pickupsituation", "star3d"]
-        for unavailablePath in unavailablePaths {
-            if path.hasPrefix(unavailablePath) {
-                return true
-            }
-        }
-        return false
-    }
-}
-
 func readLastID(allowInitialization: Bool = true) async -> Int? {
     do {
         let outputString = try String(contentsOfFile: NSHomeDirectory() + "/Library/Containers/GreatdoriOfflineResBundleGen/LastID.txt", encoding: .utf8).replacingOccurrences(of: "\n", with: "")
@@ -85,4 +63,69 @@ func fetchNewestID() async -> Int? {
 }
 
 
-
+func analyzePathBranch(_ path: String) -> String {
+    if pathIsInUnavailableBranch(path) {
+        return "unsupported"
+    } else if path.hasPrefix("video") {
+        return "video"
+    } else if path.hasPrefix("sound") {
+        return "sound"
+    } else {
+        return "basic"
+    }
+    
+    func pathIsInUnavailableBranch(_ path: String) -> Bool {
+        let unavailablePaths = [
+            "characters/ingameresourceset",
+            "live2d",
+            "musicscore",
+            "pickupsituation",
+            "star3d",
+            "additional_music",
+            "ani_degree_aniver_8.5th_rip",
+            "animationbg",
+            "appeal",
+            "changedstamp",
+            "character_name_rip",
+            "character_profile_data_rip",
+            "characterprofile",
+            "commenthomebanner_rip",
+            "effect",
+            "eventcommon_rip",
+            "friendinvite",
+            "genericanimation",
+            "graphicalinfo",
+            "homebanner_rip",
+            "limitedmission",
+            "limitedpage",
+            "loading",
+            "map",
+            "memorial",
+            "multiplay",
+            "newsituationintroduction_rip",
+            "newyearholidays",
+            "popipa_10th_rip",
+            "speciallottery",
+            "specialtraining",
+            "starshop",
+            "thumb/billinggoods",
+            "thumb/characterrank_exp_rip",
+            "thumb/costume3ddress",
+            "thumb/costume3dhairstyle",
+            "thumb/limiteditem_rip",
+            "thumb/selfintroductionepisode_rip",
+            "thumbnail",
+            "title",
+            "tutorial_rip",
+            "worldmap_rip",
+            "april",
+            "button_"
+        ]
+        for unavailablePath in unavailablePaths {
+            if path.hasPrefix(unavailablePath) {
+                return true
+            }
+        }
+        return false
+    }
+}
